7b1617d16b1931071d5f7a19ebb5a6b5fa5ccb2e
dbrent-amazon <dbrent@amazon.com>
2014-11-18 14:51:39 -0800

    Initial commit.

---
+++ src/OffAmazonPaymentsService/OffAmazonPaymentsService.config.inc.php
@@ -0,0 +1,18 @@
+<?php
+$merchantId='';
+$accessKey='';
+$secretKey='';
+
+/********************************************************************
+ * Service endpoints for OffAmazonPaymentsService
+ * 
+ * Swap between live and sandbox using the urls defined below
+ *******************************************************************/
+$region='US'; // DE, UK or US
+$environment='SANDBOX'; // SANDBOX or LIVE
+$applicationName='';
+$applicationVersion='';
+//$caBundleFile='/etc/conf/certs/caCertFile.crt'; // Uncomment to enable caCertFile for unix
+//$caBundleFile='C:\certs\caCertFile.crt';  // Uncomment to enable caCertFile for windows
+$clientId ='';
+?>
